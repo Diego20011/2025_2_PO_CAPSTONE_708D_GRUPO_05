e07a6e3b1ad474ea084464a332aad14855eb5264
PekeDeusS <cr.barrosp@gmail.com>
2025-11-13 12:29:33 -0300

    cambio paleta de colores

--- Fase2/Evidencias_Proyecto/Evidencias_de_sistema/djangoproject_caninoslook/djangoproject_caninoslook/settings.py
+++ Fase2/Evidencias_Proyecto/Evidencias_de_sistema/djangoproject_caninoslook/djangoproject_caninoslook/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "reservas/static"]
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
